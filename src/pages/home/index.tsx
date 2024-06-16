1032870542524223a0bd49dfc514edf7c13cbcd9
parth <parthhjuneja@gmail.com>
2024-06-17 02:01:58 +0530

    CREATE: hero component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,33 @@
 import Footer from '../../components/Footer';
 import Navbar from '../../components/Navbar';
+import hero from '../../assets/hero2.jpeg';
+
+
+const Hero = () => {
+    return (
+        <div className='flex flex-col-reverse md:flex-row w-full justify-between md:items-center h-fit'>
+            <div className="hero-bio md:mr-12 mt-12 md:mt-0 md:py-24 flex flex-col justify-center h-full w-full md:w-2/3">
+                <h1 className="text-primary text-3xl md:text-6xl tracking-wider font-bold !leading-tight font-mono mb-5">
+                    Hey, it's Parth.
+                </h1>
+                <p className='text-primary text-xl md:text-2xl !leading-8 md:tracking-wide font-sans !text-left mb-5 pr-2'>
+                    Software Developer building scalable solutions that are innovative, future-driven & user-centric for a progressive growth.
+                    🌱
+                </p>
+            </div>
+            <div className="hero-image w-32 h-auto md:w-52 md:h-72 left-0 overflow-clip align-middle md:mb-0">
+                <img className='rounded-t-[150px]' src={hero} alt="Hero Image" />
+            </div>
+        </div>
+    )
+}
 
 const Home = () => {
     return (
         <div className='app-wrapper px-[24px] lg:px-[126px]'>
             <div className="bg-background ">
                 <Navbar />
-                <p className="text-primary text-5xl font-bold font-mono">
-                    Hey, It's Parth
-                </p>
+                <Hero />
                 <Footer />
             </div>
         </div>
